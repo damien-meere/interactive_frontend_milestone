d13503e35d9ca24aaa5c04d605c72bfa7cafdc55
Damien Meere <damien.meere@bt.com>
2019-10-31 12:35:50 +0000

    date field added to data to facilitate easier visualisation in line charts

--- assets/js/graph_final.js
+++ assets/js/graph_final.js
@@ -78,9 +78,20 @@ function showTrainingTypePie(ndx){
     dc.pieChart('#pieTest')
         .height(400)
         .radius(200)
+        .width(400)
+        .innerRadius(80)
         .transitionDuration(1500)
+        .legend(dc.legend())
         .dimension(type_dim)
         .group(total_hours_per_type);
+
+
+}
+
+function showHoursByMonth (ndx){
+
+    var parseDate = d3.time.format("%d/%m/%Y").parse;
+
 
 
 }
